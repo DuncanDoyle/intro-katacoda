8f4f21f6bb8233ce29a72f962ed2b6ace88425ba
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 18:03:32 +0100

    More debugging.

--- middleware/brms-loan-application/env-init.sh
+++ middleware/brms-loan-application/env-init.sh
@@ -4,3 +4,6 @@ ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubuserconten
 ssh root@host01 'echo "Importing Decision Server template into OpenShift." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift && break || sleep 2; done'
 ssh root@host01 'echo "Creating loan-demo OpenShift project." >> script.log'
+ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && oc new-project loan-demo --display-name="Loan Demo" --description="Red Hat JBoss BRMS Decision Server Loan Demo" && break || sleep 2; done'
+ssh root@host01 'echo "Creating JBoss BPM Suite OpenShift application." >> script.log'
+ssh root@host01 'for i in {1..200}; do oc new-app --name jboss-bpmsuite duncandoyle/jboss-bpmsuite:6.4 -n loan-demo && break || sleep 2; done'
