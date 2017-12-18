1134476e5950c8b840803cc3e923d19a38a7e93b
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 16:27:26 +0100

    Better handling of OpenShift project creation.

--- middleware/brms-loan-application/assets/init-scenario.sh
+++ middleware/brms-loan-application/assets/init-scenario.sh
@@ -12,10 +12,6 @@ echo
 #until docker ps | grep "jboss-brms"; do echo -n . && sleep 5; done
 #echo
 
-echo "Logging into OpenShift with 'developer' account."
-oc login -u developer -p developer
-echo
-
 #Wait for Business Central OpenShift deployment
 echo "Waiting for the JBoss BRMS workbench OpenShift Container to start"
 until oc project loan-demo &>2; do echo -n . && sleep 5; done
@@ -26,6 +22,14 @@ echo
 echo "Waiting for the BRMS workbench to become available"
 export BC_HOST=oc describe route jboss-bpmsuite | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]'
 until [ $(curl -sL -w "%{http_code}\\n" "http://$BC_HOST/business-central" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
+echo
+
+echo "Logging into OpenShift with 'developer' account."
+oc login -u developer -p developer
+echo
+
+echo "Logging into the OpenShift loan-demo project."
+oc project loan-demo
 echo
 
 echo "Enviroment ready!"
