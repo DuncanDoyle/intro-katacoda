59f2816ca69ce60d5a22c9eec02763f7bb1562b2
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 16:47:34 +0100

    More debug.

--- middleware/brms-loan-application/env-init.sh
+++ middleware/brms-loan-application/env-init.sh
@@ -3,5 +3,5 @@ ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubuserconten
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift && break || sleep 2; done'
 #ssh root@host01 'docker pull duncandoyle/jboss-brms:6.4'
 #ssh root@host01 'docker run -d -p 8001:8001 -p 8080:8080 -p 9418:9418 --name=jboss-brms duncandoyle/jboss-brms:6.4 -c brms-standalone.xml -b 0.0.0.0 -bmanagement 0.0.0.0 -Derrai.bus.enable_sse_support=false'
-ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && oc new-project loan-demo --display-name="Loan Demo" --description="Red Hat JBoss BRMS Decision Server Loan Demo" && break || sleep 2; done'
-ssh root@host01 'for i in {1..200}; do oc new-app --name jboss-bpmsuite duncandoyle/jboss-bpmsuite:6.4 -n loan-demo && break || sleep 2; done'
+#ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && oc new-project loan-demo --display-name="Loan Demo" --description="Red Hat JBoss BRMS Decision Server Loan Demo" && break || sleep 2; done'
+#ssh root@host01 'for i in {1..200}; do oc new-app --name jboss-bpmsuite duncandoyle/jboss-bpmsuite:6.4 -n loan-demo && break || sleep 2; done'
