cfa3a0c3722c4ecf7e84f38f6a3abe90050793ee
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 18:10:13 +0100

    More debugging.

--- middleware/brms-loan-application/env-init.sh
+++ middleware/brms-loan-application/env-init.sh
@@ -3,4 +3,5 @@ ssh root@host01 'echo "Importing middleware Image Streams into OpenShift." >> sc
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift && break || sleep 2; done'
 ssh root@host01 'echo "Importing Decision Server template into OpenShift." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift && break || sleep 2; done'
-ssh root@host01 'echo "Creating loan-demo OpenShift project." >> script.log'
+ssh root@host01 'echo "Logging into OpenShift as developer." >> script.log'
+ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && break || sleep 2; done'
