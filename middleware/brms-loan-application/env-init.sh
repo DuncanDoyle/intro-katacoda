8ee38266ae9c45e4dc46bc246079e178defc499a
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 18:12:30 +0100

    More debugging.

--- middleware/brms-loan-application/env-init.sh
+++ middleware/brms-loan-application/env-init.sh
@@ -5,3 +5,5 @@ ssh root@host01 'echo "Importing Decision Server template into OpenShift." >> sc
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift && break || sleep 2; done'
 ssh root@host01 'echo "Logging into OpenShift as developer." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && break || sleep 2; done'
+ssh root@host01 'echo "Creating new loan-demo project in OpenShift." >> script.log'
+ssh root@host01 'for i in {1..200}; do oc new-project loan-demo --display-name="Loan Demo" --description="Red Hat JBoss BRMS Decision Server Loan Demo" && break || sleep 2; done'
