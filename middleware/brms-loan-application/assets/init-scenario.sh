0712f76870e67b7672d72b57e83ee32f04ad1ef3
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 16:42:59 +0100

    Debug OpenShift startup.

--- middleware/brms-loan-application/assets/init-scenario.sh
+++ middleware/brms-loan-application/assets/init-scenario.sh
@@ -13,23 +13,23 @@ echo
 #echo
 
 #Wait for Business Central OpenShift deployment
-echo "Waiting for the JBoss BRMS workbench OpenShift Container to start"
-until oc project loan-demo &>2; do echo -n . && sleep 5; done
-oc rollout status dc/jboss-bpmsuite -n loan-demo
-echo
+#echo "Waiting for the JBoss BRMS workbench OpenShift Container to start"
+#until oc project loan-demo &>2; do echo -n . && sleep 5; done
+#oc rollout status dc/jboss-bpmsuite -n loan-demo
+#echo
 
 #Wait for BRMS workbench availability
-echo "Waiting for the BRMS workbench to become available"
-export BC_HOST=oc describe route jboss-bpmsuite | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]'
-until [ $(curl -sL -w "%{http_code}\\n" "http://$BC_HOST/business-central" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
-echo
+#echo "Waiting for the BRMS workbench to become available"
+#export BC_HOST=oc describe route jboss-bpmsuite | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]'
+#until [ $(curl -sL -w "%{http_code}\\n" "http://$BC_HOST/business-central" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
+#echo
 
-echo "Logging into OpenShift with 'developer' account."
-oc login -u developer -p developer
-echo
+#echo "Logging into OpenShift with 'developer' account."
+#oc login -u developer -p developer
+#echo
 
-echo "Logging into the OpenShift loan-demo project."
-oc project loan-demo
-echo
+#echo "Logging into the OpenShift loan-demo project."
+#oc project loan-demo
+#echo
 
 echo "Enviroment ready!"
