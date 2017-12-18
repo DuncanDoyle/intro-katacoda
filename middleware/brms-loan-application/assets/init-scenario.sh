9b6e7914b8381e606ddfd5de6e6f3f94bc66e70b
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-18 16:04:55 +0100

    Moving to Business Central on OpenShift to make the scenario easier to consume.

--- middleware/brms-loan-application/assets/init-scenario.sh
+++ middleware/brms-loan-application/assets/init-scenario.sh
@@ -3,22 +3,28 @@ echo "Initialing environment... This may take a couple of moments."
 echo
 
 #Wait for the brms image to be pulled
-echo "Waiting for BRMS Docker Image to be pulled."
-until docker images duncandoyle/jboss-brms | grep "6.4"; do  echo -n . && sleep 5; done
-echo
+#echo "Waiting for BRMS Docker Image to be pulled."
+#until docker images duncandoyle/jboss-brms | grep "6.4"; do  echo -n . && sleep 5; done
+#echo
 
 #Wait for Container image to start
-echo "Waiting for BRMS Docker Container to start"
-until docker ps | grep "jboss-brms"; do echo -n . && sleep 5; done
+#echo "Waiting for BRMS Docker Container to start"
+#until docker ps | grep "jboss-brms"; do echo -n . && sleep 5; done
+#echo
+
+echo "Logging into OpenShift with 'developer' account."
+oc login -u developer -p developer
+echo
+
+#Wait for Business Central OpenShift deployment
+echo "Waiting for the JBoss BRMS workbench OpenShift Container to start"
+oc rollout status dc/jboss-bpmsuite -n loan-demo
 echo
 
 #Wait for BRMS workbench availability
 echo "Waiting for the BRMS workbench to become available"
-until [ $(curl -sL -w "%{http_code}\\n" "http://localhost:8080/business-central" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
-echo
-
-echo "Logging into OpenShift with 'developer' account."
-oc login -u developer -p developer
+export BC_HOST=oc describe route jboss-bpmsuite | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]'
+until [ $(curl -sL -w "%{http_code}\\n" "http://$BC_HOST/business-central" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
 echo
 
 echo "Enviroment ready!"
