3a19accbc7540ab3b3b5b65f03fa1aa21d9d9524
Duncan Doyle <ddoyle@redhat.com>
2018-02-12 22:43:27 +0100

    Working on HTTPS issue.

--- middleware/brms-loan-application/set-env.sh
+++ middleware/brms-loan-application/set-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/\.environments\.katacoda\.com/.rhdm7-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
 #~/.launch.sh
 
 ~/.init/init-scenario.sh
