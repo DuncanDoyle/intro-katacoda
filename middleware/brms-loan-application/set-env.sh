d8f07586b634869eea10f5508402741e36ddc6e3
Duncan Doyle <ddoyle@redhat.com>
2018-02-22 17:37:51 +0100

    Fixing https upgrade issue.

--- middleware/brms-loan-application/set-env.sh
+++ middleware/brms-loan-application/set-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
-
 until [ -f /openshift.local.config/master/master-config.yaml ] ; do echo -n . && sleep 5; done
-sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+#sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
 #~/.launch.sh
 
 ~/.init/init-scenario.sh
