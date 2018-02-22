8678aefea054044a505ceb7a90e65725c72e407f
Duncan Doyle <ddoyle@redhat.com>
2018-02-22 17:03:41 +0100

    Added logic to wait for file to exist.

--- middleware/brms-loan-application/set-env.sh
+++ middleware/brms-loan-application/set-env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
-#sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
-#/openshift.local.config/master/master-config.yaml
+#sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+
+until [ -f /openshift.local.config/master/master-config.yaml ] ; do echo -n . && sleep 5; done
+sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
 #~/.launch.sh
 
 ~/.init/init-scenario.sh
