be9eb38045339644636f713ace5424413470bc5a
Duncan Doyle <ddoyle@redhat.com>
2018-02-22 16:54:51 +0100

    Reset sed command to previous approach.

--- middleware/brms-loan-application/set-env.sh
+++ middleware/brms-loan-application/set-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
-sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
-
+sed -i 's/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/g' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+#sed -i '213s/\.environments\.katacoda\.com/.openstack-environments.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+#/openshift.local.config/master/master-config.yaml
 #~/.launch.sh
 
 ~/.init/init-scenario.sh
